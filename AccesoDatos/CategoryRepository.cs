d9e5b9b4a34ce9a6f2af39a6015b5732df5f570c
AndreaN <nm0753032023@unab.edu.sv>
2024-10-13 21:40:42 -0600

    Agregar archivos de proyecto.

---
+++ AccesoDatos/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class CategoryRepository
+    {
+        public NorthwindEntities contexto = new NorthwindEntities();
+
+        public List<Categories> Obtener()
+        {
+            var categoria = from cat in contexto.Categories select cat;
+            return categoria.ToList();
+        }
+
+        public Categories ObtenerPorID(int id)
+        {
+            var categoria = from cat in contexto.Categories where cat.CategoryID == id select cat;
+            return categoria.FirstOrDefault();
+        }
+
+        public int DeleteCategoria(int id)
+        {
+            var registro = ObtenerPorID(id);
+            if (registro != null)
+            {
+                contexto.Categories.Remove(registro);
+                return contexto.SaveChanges();
+            }
+            return 0;
+        }
+    }
+}
---
+++ EFEliminar/Form1.cs
@@ -0,0 +1,62 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFEliminar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private CategoryRepository repository = new CategoryRepository();
+
+        private void btnObtenerTodos_Click(object sender, EventArgs e)
+        {
+            var categoria = repository.Obtener();
+            dgvCategory.DataSource = categoria;
+        }
+
+        private void btnTodos_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtObtenerTodos.Text);
+            var categoria = repository.ObtenerPorID(id);
+            List<Categories> lista1 = new List<Categories> { categoria };
+            if (categoria != null)
+            {
+                llenarCampos(categoria);
+            }
+            dgvCategory.DataSource = lista1;
+        }
+
+        private void llenarCampos(Categories categories)
+        {
+            tbxCategoryID.Text = categories.CategoryID.ToString();
+            tbxCategoryName.Text = categories.CategoryName;
+            tbxDescription.Text = categories.Description;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbxCategoryID.Text, out int categoryID))
+            {
+                var eliminadas = repository.DeleteCategoria(categoryID);
+
+                MessageBox.Show($"Se ha eliminado {eliminadas} filas ");
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un ID válido, porfax.");
+            }
+        }
+    }
+}
